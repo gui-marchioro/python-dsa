e07ee6bf0799d9792aa3c98ef758805c7c227ae2
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-12-07 22:37:32 -0300

    Quick sort algorithm

---
+++ tests/sorting/test_quick_sort.py
@@ -0,0 +1,18 @@
+import pytest
+from sorting.quick_sort import quicksort
+
+
+@pytest.mark.parametrize(
+    "input_array, expected_output",
+    [
+        ([4, 2, 5, 1], [1, 2, 4, 5]),
+        ([1, 2, 3, 4], [1, 2, 3, 4]),
+        ([5, 4, 3 , 2, 1], [1, 2, 3, 4, 5]),
+        ([3, 1, 4, 2, 5], [1, 2, 3, 4, 5]),
+        ([], []),
+        ([1], [1]),
+        ([2, 1], [1, 2]),
+    ]
+)
+def test_bubble_sort(input_array, expected_output):
+    assert quicksort(input_array) == expected_output
